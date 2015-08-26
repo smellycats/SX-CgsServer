9bd823604d98959ce3058ec02928a3871612c295
SmellyCat <smellycat2014@foxmail.com>
2015-08-26 14:58:44 +0800

    Add mysql models

--- cgs/models.py
+++ cgs/models.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
+import json
 
 from peewee import *
 
-from app import db
+from app import db, mysql_db
 
 
 class BaseModel(Model):
-    class Meta:
-        database = db
-
+    
     @classmethod
     def get_one(cls, *query, **kwargs):
         # 为了方便使用，新增此接口，查询不到返回None，而不抛出异常
@@ -17,6 +16,15 @@ class BaseModel(Model):
         except DoesNotExist:
             return None
 
+class JSONModel(BaseModel):
+    def __str__(self):
+        r = {}
+        for k in self._data.keys():
+            try:
+                r[k] = str(getattr(self, k))
+            except:
+                r[k] = json.dumps(getattr(self, k))
+        return str(r)
 
 class Users(BaseModel):
     username = CharField(unique=True)
@@ -24,3 +32,27 @@ class Users(BaseModel):
     date_created = DateTimeField()
     date_modified = DateTimeField()
     banned = BooleanField(default=False)
+
+    class Meta:
+        database = db
+
+class Vehicle_gd(BaseModel):
+    hpzl = CharField()
+    hphm = CharField()
+    clpp1 = CharField()
+    clxh = CharField()
+    clpp2 = CharField()
+    gcjk = CharField()
+    zzg = CharField()
+    zzcmc = CharField()
+    clsbdh = CharField()
+    fdjh = CharField()
+    cllx = CharField()
+    csys = CharField()
+    sfzmhm = CharField()
+    sfzmmc = CharField()
+    syr = CharField()
+    fzrq = CharField()
+    class Meta:
+        database = mysql_db
+
