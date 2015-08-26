294f169a7620a8e3f93debd81bc19c27b940bdab
SmellyCat <smellycat2014@foxmail.com>
2015-08-26 14:58:32 +0800

    Add url parse funcs

--- cgs/help_func.py
+++ cgs/help_func.py
@@ -1,28 +1,32 @@
 # -*- coding: utf-8 -*-
+import urllib
 
-def get_hpzl_cpzl(hphm, hpys):
-    if hphm == None or hphm == '-' or hphm == '':
-        return (hphm, '00' , '')
-    if hphm[:1]== u'粤':
-        if hphm[-1] == u'学':
-            return (hphm[1:-1], '16', u'标准车牌')
-        if hphm[-1] == u'挂':
-            return (hphm[1:-1], '15', u'标准车牌')
-        if hpys == 2:
-            return (hphm[1:], '02', u'标准车牌')
-        if hpys == 3:
-            return (hphm[1:], '01', u'双层车牌')
-        if hpys == 5:
-            return (hphm[1:], '06', u'标准车牌')
-    return (hphm, '00', u'标准车牌')
-    
-    
-def test():
-    s = u'粤L12345学'
-    print s[1:-1]
 
+def url_decode(query):
+    d = {}
+    params_list = query.split('&')
+    for i in params_list:
+        if i.find('=') >= 0:
+            k,v = i.split('=', 1)
+            d[k] = v
+    return d
+
+def q_decode(q):
+    d = {}
+    q_list = q.split('+')
+    d['q'] = q_list[0]
+    for i in q_list[1:]:
+        if i.find(':') >= 0:
+            k,v = i.split(':', 1)
+            d[k] = v
+    return d
 
 if __name__ == "__main__":
-    s = get_hpzl(u'粤L12345', 5)
-    print s[0], s[1]
+    #s = get_hpzl(u'粤L12345', 5)
+    #print s[0], s[1]
+    #print urldecode(u'q%3D粤L12=34%2Bhpzl:02+cllx:K31%26order=desc')
+    query = 'q'
+    print url_decode(query);
+    #q = u'\u7ca4L12=34+hpzl:02+cllx:K31'
+    #print q_decode(q)
 
