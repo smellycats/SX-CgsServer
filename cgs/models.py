c5dd8f61775464173696ba4568e15aeded86575a
SmellyCat <smellycat2014@foxmail.com>
2015-08-29 05:04:53 +0800

    Remove peewee ,use sqlalchemy instead

--- cgs/models.py
+++ cgs/models.py
@@ -1,58 +1,104 @@
 # -*- coding: utf-8 -*-
-import json
+from datetime import datetime
 
-from peewee import *
+from app import db
 
-from app import db, mysql_db
+class Users(db.Model):
+    """用户"""
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), index=True)
+    password = db.Column(db.String(128))
+    scope = db.Column(db.String(128), default='')
+    date_created = db.Column(db.DateTime, default=datetime.now())
+    date_modified = db.Column(db.DateTime, default=datetime.now())
+    banned = db.Column(db.Integer, default=0)
+
+    def __init__(self, username, password, scope='', banned=0,
+                 date_created=None, date_modified=None):
+        self.username = username
+        self.password = password
+        self.scope = scope
+        self.date_created = datetime.now()
+        self.date_modified = datetime.now()
+        self.banned = banned
+
+    def __repr__(self):
+        return '<Users %r>' % self.id
 
 
-class BaseModel(Model):
-    
-    @classmethod
-    def get_one(cls, *query, **kwargs):
-        # 为了方便使用，新增此接口，查询不到返回None，而不抛出异常
-        try:
-            return cls.get(*query, **kwargs)
-        except DoesNotExist:
-            return None
+class Scope(db.Model):
+    """权限范围"""
+    __tablename__ = 'scope'
 
-class JSONModel(BaseModel):
-    def __str__(self):
-        r = {}
-        for k in self._data.keys():
-            try:
-                r[k] = str(getattr(self, k))
-            except:
-                r[k] = json.dumps(getattr(self, k))
-        return str(r)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
 
-class Users(BaseModel):
-    username = CharField(unique=True)
-    password = CharField()
-    date_created = DateTimeField()
-    date_modified = DateTimeField()
-    banned = BooleanField(default=False)
+    def __init__(self, name):
+        self.name = name
 
-    class Meta:
-        database = db
+    def __repr__(self):
+        return '<Scope %r>' % self.id
 
-class Vehicle_gd(BaseModel):
-    hpzl = CharField()
-    hphm = CharField()
-    clpp1 = CharField()
-    clxh = CharField()
-    clpp2 = CharField()
-    gcjk = CharField()
-    zzg = CharField()
-    zzcmc = CharField()
-    clsbdh = CharField()
-    fdjh = CharField()
-    cllx = CharField()
-    csys = CharField()
-    sfzmhm = CharField()
-    sfzmmc = CharField()
-    syr = CharField()
-    fzrq = CharField()
-    class Meta:
-        database = mysql_db
+
+class Csys(db.Model):
+    """车身颜色"""
+    __bind_key__ = 'cgs'
+    __tablename__ = 'csys'
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(1), unique=True)
+    name = db.Column(db.String(3), index=False)
+
+    def __init__(self, code, name):
+        self.code = code
+        self.name = name
+
+    def __repr__(self):
+        return '<Csys %r>' % self.id
+
+
+class VehicleGD(db.Model):
+    """车管所数据"""
+    __bind_key__ = 'cgs'
+    __tablename__ = 'vehicle_gd'
+    id = db.Column(db.Integer, primary_key=True)
+    hpzl = db.Column(db.String(2))
+    hphm = db.Column(db.String(16))
+    clpp1 = db.Column(db.String(32))
+    clxh = db.Column(db.String(64))
+    clpp2 = db.Column(db.String(64))
+    gcjk = db.Column(db.String(4))
+    zzg = db.Column(db.String(32))
+    zzcmc = db.Column(db.String(128))
+    clsbdh = db.Column(db.String(128))
+    fdjh = db.Column(db.String(64))
+    cllx = db.Column(db.String(3))
+    csys = db.Column(db.String(4))
+    sfzmhm = db.Column(db.String(32))
+    sfzmmc = db.Column(db.String(8))
+    syr = db.Column(db.String(128))
+    fzrq = db.Column(db.String(32))
+
+    def __init__(self, hpzl, hphm, clpp1, clxh, clpp2, gcjk, zzg, zzcmc,
+                 clsbdh, fdjh, cllx, csys, sfzmhm, sfzmmc, syr, fzrq):
+        self.hpzl = hpzl
+        self.hphm = hphm
+        self.clpp1 = clpp1
+        self.clxh = clxh
+        self.clpp2 = hphm
+        self.gcjk = hphm
+        self.zzg = hphm
+        self.zzcmc = hphm
+        self.clsbdh = clsbdh
+        self.fdjh = fdjh
+        self.cllx = cllx
+        self.csys = csys
+        self.sfzmhm = sfzmhm
+        self.sfzmmc = sfzmmc
+        self.syr = syr
+        self.fzrq = fzrq
+
+    def __repr__(self):
+        return '<VehicleGD %r>' % self.id
 
