e6cd9da8f0265925f5ec6a1b186d647667a7cfa3
SmellyCat <smellycat2014@foxmail.com>
2015-08-30 02:05:38 +0800

    Add flask cache

--- cgs/views.py
+++ cgs/views.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
 import urlparse
 import urllib
 from functools import wraps
 
+import arrow
 from flask import g, request
 from flask_restful import reqparse, abort, Resource
 from passlib.hash import sha256_crypt
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
-from app import db, app, api, auth, limiter, logger
+from app import db, app, api, auth, limiter, logger, cache
 from models import Users, VehicleGD, Scope
 from help_func import *
 
@@ -23,6 +23,7 @@ def verify_password(username, password):
     if user:
         return sha256_crypt.verify(password, user.password)
     return False
+
 
 def verify_token(f):
     """token验证装饰器"""
@@ -42,14 +43,32 @@ def verify_token(f):
         return f(*args, **kwargs)
     return decorated_function
 
+@cache.memoize(50)
+def get_vehicle(hphm='', hpys=None):
+    vehicle = VehicleGD.query.filter_by(hphm=hphm)
+    if hpys:
+        hpzl = []
+        if hpys in set([u'blue', u'蓝', u'2']):
+            hpzl = ('02', '08')
+        elif hpys in set([u'yellow', u'黄', u'3']):
+            hpzl = ('01', '07', '13', '14', '15', '16', '17')
+        elif hpys in set([u'white',u'白',u'4']):
+            hpzl = ('20', '21', '22', '24', '32')
+        elif hpys in set([u'black',u'黑',u'5']):
+            hpzl = ('03', '04', '05', '06', '09', '10', '11', '12')
+        else:
+            hpzl = ()
+        vehicle = VehicleGD.query.filter(VehicleGD.hpzl.in_(hpzl))
+    return vehicle.all()
+
 
 class Index(Resource):
     def get(self):
         return {
-            'user_url': 'http://127.0.0.1:%s/user{/username}' %  app.config['PORT'],
-            'scope_url': 'http://127.0.0.1:%s/scope' %  app.config['PORT'],
-            'token_url': 'http://127.0.0.1:%s/token' %  app.config['PORT'],
-            'vehicle_url': 'http://127.0.0.1:%s/vehicle' %  app.config['PORT']
+            'user_url': 'http://127.0.0.1:%s/user{/user_id}' % app.config['PORT'],
+            'scope_url': 'http://127.0.0.1:%s/scope' % app.config['PORT'],
+            'token_url': 'http://127.0.0.1:%s/token' % app.config['PORT'],
+            'vehicle_url': 'http://127.0.0.1:%s/vehicle' % app.config['PORT']
         }, 200, {'Cache-Control': 'public, max-age=60, s-maxage=60'}
 
 
@@ -89,7 +108,7 @@ class User(Resource):
         # 求交集后的权限
         u_scope = ','.join(all_scope & request_scope)
 
-        db.session.query(Users).filter_by(id=user_id).update({'scope': u_scope, 'date_modified': datetime.now()})
+        db.session.query(Users).filter_by(id=user_id).update({'scope': u_scope, 'date_modified': arrow.now().datetime})
         db.session.commit()
 
         user = Users.query.filter_by(id=user_id).first()
@@ -100,7 +119,7 @@ class User(Resource):
                 'scope': user.scope,
                 'date_created': str(user.date_created),
                 'date_modified': str(user.date_modified),
-                'banned': user.banned}, 201    
+                'banned': user.banned}, 201
 
 
 class UserList(Resource):
@@ -160,14 +179,16 @@ class ScopeList(Resource):
 
 class TokenList(Resource):
     decorators = [limiter.limit("5/hour")]
-    
+
     def post(self):
         parser = reqparse.RequestParser()
 
         parser.add_argument('username', type=unicode, required=True,
-                            help='A username field is require', location='json')
+                            help='A username field is require',
+                            location='json')
         parser.add_argument('password', type=unicode, required=True,
-                            help='A password field is require', location='json')
+                            help='A password field is require',
+                            location='json')
         args = parser.parse_args()
 
         user = Users.query.filter_by(username=request.json['username'],
@@ -202,23 +223,10 @@ class Vehicle(Resource):
             return {'message': 'Validation Failed', 'error': error}, 422
         q_dict = q_decode(get_params['q'])
 
-        vehicle = VehicleGD.query.filter_by(hphm=q_dict['q'])
-        if q_dict.get('hpys', None):
-            hpzl = []
-            if q_dict['hpys'] in set([u'blue', u'蓝', u'2']):
-                hpzl = ('02', '08')
-            elif q_dict['hpys'] in set([u'yellow', u'黄', u'3']):
-                hpzl = ('01', '07', '13', '14', '15', '16', '17')
-            elif q_dict['hpys'] in set([u'white',u'白',u'4']):
-                hpzl = ('20', '21', '22', '24', '32')
-            elif q_dict['hpys'] in set([u'black',u'黑',u'5']):
-                hpzl = ('03', '04', '05', '06', '09', '10', '11', '12')
-            else:
-                hpzl = ()
-            vehicle = VehicleGD.query.filter(VehicleGD.hpzl.in_(hpzl))
+        vehicle = get_vehicle(q_dict['q'], q_dict.get('hpys', None))
 
         items = []
-        for i in vehicle.all():
+        for i in vehicle:
             items.append({'id': i.id, 'hpzl': i.hpzl, 'clxh': i.clxh,
                           'clpp1': i.clpp1, 'clpp2': i.clpp2, 'zzcmc': i.zzcmc,
                           'clsbdh': i.clsbdh, 'fdjh': i.fdjh, 'cllx': i.cllx,
@@ -226,7 +234,6 @@ class Vehicle(Resource):
 
         return {'total_count': len(items), 'items': items}, 200,
         {'Cache-Control': 'public, max-age=60, s-maxage=60'}
-
 
 
 api.add_resource(Index, '/')
