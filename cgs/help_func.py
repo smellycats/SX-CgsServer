49fcedc4e1af98fe8e0f806009b8fe5036354977
SmellyCat <smellycat2014@foxmail.com>
2015-08-30 02:04:40 +0800

    Update fix pep8 style

--- cgs/help_func.py
+++ cgs/help_func.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import urllib
-
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired, BadSignature
 
 
@@ -9,9 +7,10 @@ def url_decode(query):
     params_list = query.split('&')
     for i in params_list:
         if i.find('=') >= 0:
-            k,v = i.split('=', 1)
+            k, v = i.split('=', 1)
             d[k] = v
     return d
+
 
 def q_decode(q):
     d = {}
@@ -19,22 +18,25 @@ def q_decode(q):
     d['q'] = q_list[0]
     for i in q_list[1:]:
         if i.find(':') >= 0:
-            k,v = i.split(':', 1)
+            k, v = i.split(':', 1)
             d[k] = v
     return d
+
 
 def verify_auth_token(token, key):
     s = Serializer(key)
     try:
         return s.loads(token)
     except SignatureExpired:
-        return 'expired' # valid token, but expired
+        # valid token, but expired
+        return 'expired'
     except BadSignature:
-        return None # invalid token
+        # invalid token
+        return None
+
 
 def row2dict(row):
     d = {}
     for col in row.__table__.columns:
         d[col.name] = getattr(row, col.name)
     return d
-
