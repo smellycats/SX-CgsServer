6d75651e9ad734444aab205a5aedaf8c2bf3a0f5
SmellyCat <smellycat2014@foxmail.com>
2015-09-10 17:51:00 +0800

    Add host in app.run use '/' instead of '\\'

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from cgs import app, views
 from cgs import debug_logging, online_logging, access_logging
 
 if __name__ == '__main__':
-    debug_logging(u'logs\\error.log')
-    access_logging(u'logs\\access.log')
-    app.run(port=8098, threaded=True)
+    debug_logging(u'logs/error.log')
+    access_logging(u'logs/access.log')
+    app.run(host='0.0.0.0', port=8098, threaded=True)
